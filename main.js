0c5494a3d9223bb73d5cb822cd4716712f7b72af
Nimira43 <n.rabone72@aol.com>
2025-12-14 22:14:29 +0000

    Refactored main.js

--- main.js
+++ main.js
@@ -1,41 +1,21 @@
-import { DELAY_AI, GRID_CIRCLE, GRID_COLS, GRID_ROWS, MARGIN, COLOUR_BG, COLOUR_FRAME, COLOUR_FRAME_BOTTOM,COLOUR_RI, COLOUR_RI_DARK, COLOUR_AI, COLOUR_AI_DARK, COLOUR_TIE, COLOUR_TIE_DARK, COLOUR_WIN, TEXT_RI, TEXT_AI, TEXT_TIE, TEXT_WIN } from './js/constants.js'
+import './js/constants.js'
+import { state } from './js/state.js'
+import { click, highlightGrid } from './js/input.js'
+import { setDimensions, playGame, newGame } from './js/game.js'
 
-const canvasEl = document.querySelector('canvas')
-const ctx = canvasEl.getContext('2d')
+state.canvasEl = document.querySelector('canvas')
+state.ctx = state.canvasEl.getContext('2d')
 
+state.canvasEl.addEventListener('click', (e) => {
+  if (state.gameOver) {
+    newGame()
+  } else {
+    click(e)
+  }
+})
 
-
-let gameOver,
-    gameTied,
-    grid = [],
-    playersTurn,
-    timeAI
-
-let width,
-    height,
-    margin
-setDimensions()
-
-canvasEl.addEventListener('click', click)
-canvasEl.addEventListener('mousemove', highlightGrid)
+state.canvasEl.addEventListener('mousemove', highlightGrid)
 window.addEventListener('resize', setDimensions)
 
-let timeDiff, timeLast
+setDimensions()
 requestAnimationFrame(playGame)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
