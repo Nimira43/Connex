d903d05cbdfe7635c514acce099367f12c33fb57
Nimira43 <n.rabone72@aol.com>
2025-12-14 21:20:23 +0000

    Wrapped all the globals in a single shared state object

---
+++ js/state.js
@@ -0,0 +1,13 @@
+export const state = {
+  gameOver: false,
+  gameTied: false,
+  grid: [],
+  playersTurn: true,
+  timeAI: 0,
+  width: 0,
+  height: 0,
+  margin: 0,
+  canvasEl: null,
+  ctx: null,
+  timeLast: null,
+}
